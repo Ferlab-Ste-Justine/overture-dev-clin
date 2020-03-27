749ab29d53160f4c945ad388084948fa68c79f36
Eric Vallee <eric_vallee@webificservices.com>
2020-03-27 18:03:31 -0400

    Moved aidbox setup to a separate script to more easily satisfy clin's aidbox requirement

--- setup_elasticsearch.sh
+++ setup_elasticsearch.sh
@@ -9,16 +9,7 @@ else
 fi
 
 if [ ! -d clin-etl-FHIR/ndjson ]; then
-    #Import spreadsheet dummy data in a Fhir database
-    (
-        cd fhir-import;
-        cd aidbox; 
-        docker-compose up -d;
-        sleep 20;
-        cd ..;
-        docker build -t fhir-import:latest .;
-        docker run --rm --network aidbox -e "AIDBOX_URL=http://devbox:8888" -e "AIDBOX_AUTH_TOKEN=cm9vdDpzZWNyZXQ=" fhir-import:latest /bin/bash -c "/opt/upsert.sh";
-    )
+    ./launch_aidbox.sh;
 
     #Dump content of Fhir database in ndjson files
     (
